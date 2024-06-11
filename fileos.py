084df150928b3baab148f72733cdfa4f76d6f1c3
eddie <tjdals864237@gmail.com>
2024-06-11 23:58:27 +0900

    OS Module

--- fileos.py
+++ fileos.py
@@ -1,1 +1,26 @@
 import os
+
+files = os.listdir('fileos_test') #relative directory
+files = os.listdir('C:\Users\CKIRUser\Desktop\coding_eddie') #absolute directory
+
+for i in os.listdir('fileos_test'):
+    if 'jpg' in i:
+        os.rename(f'fileos_test/{i}', f'fileos_test/a{i}')
+
+for i in os.listdir('fileos_test'):
+    os.rename(r'C:\Users\CKIRUser\Desktop\coding_eddie' + f'fileos_test/{i}', f'fileos_test/a{i}')
+
+
+os.path.join('path1', 'path2')
+
+for i in os.listdir('fileos_test'):
+    os.rename(os.path.join(r'C:\Users\CKIRUser\Desktop\coding_eddie', f'fileos_test/{i}'), f'fileos_test/a{i}')
+
+os.getcwd() #absolute directory
+
+import shutil
+
+for i in os.listdir('fileos_test'):
+    if 'jpg' in i:
+        shutil.copy(f'fileos_test/{i}', f'fileos_test2/{i}')
+    

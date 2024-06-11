1c170253feba99f5bc969a4298726b127b628344
eddie <tjdals864237@gmail.com>
2024-06-11 23:42:04 +0900

    Naver Blog Post

--- naver.py
+++ naver.py
@@ -11,6 +11,7 @@ options.add_argument(r'user-driver-dir=C:\Users\CKIRUser\AppData\Local\Google\Ch
 
 driver = webdriver.Chrome(options=options)
 driver.get('https://nid.naver.com/nidlogin.login?svctype=262144')
+driver.implicitly_wait(10)
 
 time.sleep(2)
 
@@ -27,5 +28,25 @@ e.send_keys(Keys.CONTROL, 'v')
 time.sleep(1)
 
 e.send_keys(Keys.ENTER)
+time.sleep(1)
 
+# e = driver.find_element(By.CSS_SELECTOR, '#new.dontsave').click()
+# time.sleep(2)
+
+driver.get('https://m.blog.naver.com/FeedList.naver')
+time.sleep(1.5)
+
+driver.get('https://blog.editor.naver.com/editor?deviceType=mobile&returnUrl=https%3A%2F%2Fm.blog.naver.com%2FGoWriteForm.naver')
+time.sleep(1.5)
+
+e = driver.find_element(By.CSS_SELECTOR, 'textarea[placeholder="제목"]') #Better to be specific
+e.send_keys('Testing')
+
+e = driver.find_element(By.CSS_SELECTOR, 'div[placeholder="내용을 입력하세요."]') #Better to be specific
+e.send_keys('Code\nIn progress')
+e.send_keys(Keys.ENTER)
+e.send_keys('hehe')
+# publish
+# e = driver.find_element(By.CSS_SELECTOR, 'editor-header .btn_applyPost').click()
 time.sleep(1000000)
+
